c0aed3fc0d048321260e17a98fe1fb74cf31dfff
otero9 <adrianoterotic@gmail.com>
2020-05-07 12:50:52 +0200

    Eliminado reestructuracion carpetas

--- app/src/main/java/com/udc/master/tfm/tracksports/locationtracker/ProviderType.java
+++
@@ -1,8 +0,0 @@
-package com.udc.master.tfm.tracksports.locationtracker;
-
-/**
- * Tipos de proveedores de localizacion utilizados
- * @author a.oteroc
- *
- */
-public enum ProviderType {NETWORK, GPS};
